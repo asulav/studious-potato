895ccdd85ce76db6988b7e5538fcae63324a83b9
Nitesh Parajuli <np6hd@virginia.edu>
2020-12-27 21:09:08 -0500

    a little more change

--- FamilyTree/core/models.py
+++ FamilyTree/core/models.py
@@ -15,17 +15,25 @@ class ConnectDB:
             "CREATE (p:Person) {name: $user.name, age: $user.age}")
 
     def createRelation(tx, user):
+        # from front end get the first and second user to set their relation ship
         s = tx.run("MATCH (p1:Person {name: $user.firstUser}" +
                    "MATCH (p2:Person {name: $user.secondUser})" +
-                   "CREATE (p1)-[rel:$user.relationType]->($p2))")
+                   "CREATE (p1)-[rel:$user.relationType]->(p2))")
+        pass
+
+    def reorderRelation(tx, graph):
+        # do we need to reorder the tree so that the root
+        # is the oldest member of the family
         pass
 
     def removeRelation(tx, user):
-        # to do later
+        s = tx.run(
+            "MATCH (p1:Person {name: $user.firstUser})-[r:$user.relationType]->(p2:Person {name: $user.secondUser})" +
+            "DELETE r")
         pass
 
-
     def printGraph():
+        # how to traverse this tree and show to front end?
         # to do later
         # root -> traverse tree -> print
         pass
