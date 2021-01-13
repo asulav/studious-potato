54dd8f5140287157f4143950643ef9cbf61159ca
Nitesh Parajuli <np6hd@virginia.edu>
2021-01-12 21:05:57 -0500

    windows-linux issue

--- FamilyTree/core/views.py
+++ FamilyTree/core/views.py
@@ -10,16 +10,17 @@ class GetNodesData(APIView):
     def get(self, request):
         name = request.GET.get('name')
         person = Person.nodes.get_or_none(name=name)
-        return Response(person)
+        return Response(person, status=status.HTTP_200_OK)
 
 
 class GetAllNodes(APIView):
     def get(self, request):
-        return Response(Person.nodes.all())
+        return Response(Person.nodes.all(), status=status.HTTP_200_OK)
 
 
 class CreateNode(APIView):
     def post(self, request):
+        print("test")
         data = json.loads(request.body)
         name = data['name']
         age = int(data['age'])
@@ -28,7 +29,7 @@ class CreateNode(APIView):
             response = Person(name=name, age=age).save()
         except:
             response = {"error": "Person already exists"}
-        return Response(response)
+        return Response(response, status=status.HTTP_200_OK)
 
 
 class DeleteNode(APIView):
@@ -40,7 +41,7 @@ class DeleteNode(APIView):
             response = Person.nodes.get(name=name).delete()
         except:
             response = {"error": "Person not found"}
-        pass
+        return Response(response, status=status.HTTP_200_OK)
 
 
 class CreateRelations(APIView):
--- FamilyTree/scripts/entrypoint.sh
+++ FamilyTree/scripts/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 source .env
+python manage.py install_labels
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
