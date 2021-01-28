8b2ea50dbd3dd0071b69d3662af18d49db913af6
Nitesh Parajuli <np6hd@virginia.edu>
2021-01-28 08:54:46 -0500

    new property defined

--- FamilyTree/core/models.py
+++ FamilyTree/core/models.py
@@ -1,30 +1,12 @@
 #from django.db import models
 from neomodel import StructuredNode, StringProperty, IntegerProperty
-from neomodel import UniqueIdProperty, RelationshipTo
-from neomodel.properties import ArrayProperty
-
-class RelationshipMixin(object):
-    def __init__(self):
-        self.all = {}
-
-    def add_relationship(self, model, rel_type):
-        if None in (model, rel_type):
-            raise ValueError(f"RelationShip Types cannot be null")
-        self.all[rel_type] = RelationshipTo(model, rel_type).save()
-
-    def all(self):
-        return self.all
-
-    def connect(self, rel_type, rel_object):
-        try:
-            temp_rel = self.all[rel_type]
-            temp_rel.manager.connect(rel_object).save()
-        except KeyError:
-            raise KeyError("The given relation could not be found")
+from neomodel import UniqueIdProperty, Relationship, RelationshipTo
+import RelationshipProperty
 
 class Picture(StructuredNode):
     uid = UniqueIdProperty()
     location = StringProperty(required=True)
+
 
 class Person(StructuredNode):
     uid = UniqueIdProperty()
@@ -32,13 +14,22 @@ class Person(StructuredNode):
     lastName = StringProperty(required=True)
     age = IntegerProperty(default=0)
     picture = RelationshipTo('Picture', 'pic_location')
-    relationships = RelationshipMixin()
+    relations = RelationshipProperty()
+
+    def add_relationship(self, rel_type):
+        self.relations[rel_type] = RelationshipTo("Person", rel_type)
+        self.save()
+
+    def connect(self, rel_type, rel_object):
+        try:
+            temp_rel = self.relations[rel_type]
+            temp_rel.connect(rel_object)
+            self.save()
+        except KeyError:
+            raise KeyError("The given relation could not be found")
 
     def get_name(self):
         return f"{self.firstName.capitalize()} {self.lastName.capitalize()}"
-    
+
     def get_picture_location(self):
         return f"{self.picture.location}" if self.picture.location else None
-
-    def add_personal_relationship(self, type):
-        self.relationships.add_relationship("Person", type)
